74c096c4d3fc164e711a1ef8613ec8d4ba65b363
Bruno <bruno-amil@hotmail.com>
2021-11-17 02:39:46 -0300

    configurando theme, storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
-import GlobalStyles from '../src/styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
+import GlobalStyles from 'styles/global';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -13,8 +15,10 @@ export const parameters = {
 export const decorators = [
   (Story) => (
     <>
+      <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
+      </ThemeProvider>
     </>
   )
 ]
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,27 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
 
 import GlobalStyles from 'styles/global';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon.png" />
-        <link rel="apple-touch-icon" href="/img/icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="Um projeto simples com TypeScript, React, NextJS and Styled Components"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Boilerplate</title>
+          <link rel="shortcut icon" href="/img/icon.png" />
+          <link rel="apple-touch-icon" href="/img/icon.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="Um projeto simples com TypeScript, React, NextJS and Styled Components"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
